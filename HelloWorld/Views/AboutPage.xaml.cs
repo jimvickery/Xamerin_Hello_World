0c3fcd11631f1c1bc2fbc0aeaeaf5e64bc4a9a04
Jim Vickery <jimrv8@yahoo.com>
2018-02-25 22:23:21 -0600

    first commit

---
+++ HelloWorld/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Xamerin_Hello_World
